e40c4326883b3d3ed2b9c9c87f2271ffeb2de428
Mohit Bari <mohitbari8@gmail.com>
2026-07-21 21:04:52 +0530

    new update By Mohit

--- src/main/java/com/cdac/cdachub/repository/UserRepository.java
+++ src/main/java/com/cdac/cdachub/repository/UserRepository.java
@@ -8,3 +8,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByGoogleId(String googleId);
 }
+// how this was working 
+// 1. User logs in with Google OAuth2
+// email and googlr id was not default curd
+// so how spring data jpa does this
+// 1. Spring Data JPA looks at the method na
+// me and infers the query from it. In this case, 
+//findByEmail means it will look for a User entity where the email field matches the provided email parameter.
